e1a32b1dad83b4f2a73ccf7960a8f9d4cf5aaaf9
Zoya Torosyan <javahk98@bk.ru>
2024-03-14 21:12:37 +0300

    Init

---
+++ StringTask.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string message("Town of friendly people");
+    std::cout << message.length() << "\n";
+    std::cout << message.front() << "\n";
+    std::cout << message.back();
+}
